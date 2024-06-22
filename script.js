66db8c38a47854471f1c2186ab569cfbfe217f72
Shifa-M312 <shifanarasheedm@gmail.com>
2024-06-22 05:38:37 +0530

    my new experience in git

---
+++ script.js
@@ -0,0 +1,16 @@
+var a = +prompt("Enter first number");
+var b = +prompt("Enter second number");
+alert(a + b);
+
+
+
+
+// let sum=0
+// for( let i=1; i<=10; i++){
+//     if(i%2===0){
+//         console.log(i,'is an even number');
+//     }else{
+//         console.log(i,'is an odd number')
+//     }
+//     }
+    // console.log(sum)
