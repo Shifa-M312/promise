f404a1f373b2fb4cd8be7cf09a1610a9e846d096
Shifa-M312 <shifanarasheedm@gmail.com>
2024-06-22 06:07:43 +0530

    my new experience in git

---
+++ area.js
@@ -0,0 +1,4 @@
+let length = parseInt(prompt("Type the length of a Rectangle: "));
+let width = parseInt(prompt("Type the width of a Rectangle: "));
+let solving_area = length * width;
+console.log("The area of the rectangle is " + solving_area + ".");
---
+++ assignment-old/script.js
@@ -0,0 +1,7 @@
+var a = +prompt("Enter first number");
+var b = +prompt("Enter second number");
+alert(a + b);
+
+
+
+
